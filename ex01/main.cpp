87c150d3852799db0aace65d849d0aa7ddc67241
Abdellatif El Fagrouch <ael-fagr@e1r7p14.1337.ma>
2025-02-15 15:26:27 +0100

    my cpp)$

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-fagr <ael-fagr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 17:46:34 by ael-fagr          #+#    #+#             */
-/*   Updated: 2025/02/12 18:24:19 by ael-fagr         ###   ########.fr       */
+/*   Updated: 2025/02/15 15:20:37 by ael-fagr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ int main()
 {
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+
+    j = i;
+    std::cout << "Address of i is " << &i << std::endl;
+    std::cout << "Address of j is " << &j << std::endl;
+    i->makeSound();
+    j->makeSound();
     delete j;
     delete i;
     return 0;
